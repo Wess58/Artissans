89ea8a0e4fd3416a1cd739cef0a6bb2590b44066
Wess58 <wesleymbate@gmail.com>
2018-11-04 16:11:42 +0300

    Used BindView & ButterKnife

--- app/src/main/java/com/wess58/artissans/LogInActivity.java
+++ app/src/main/java/com/wess58/artissans/LogInActivity.java
@@ -2,12 +2,26 @@ package com.wess58.artissans;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LogInActivity extends AppCompatActivity {
+
+    @BindView(R.id.Username) EditText mUsername;
+    @BindView(R.id.editText2) EditText mPassword;
+    @BindView(R.id.button) Button mLoginButton;
+    @BindView(R.id.checkBox) CheckBox mStayLoggedin;
+    @BindView(R.id.loginText) TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        ButterKnife.bind(this);
     }
 }
