57f616d5c07c6e48d4efab0eff4ebdea10e6fad3
Wess58 <wesleymbate@gmail.com>
2018-11-04 16:10:52 +0300

    Used BindView & ButterKnife

--- app/src/main/java/com/wess58/artissans/SignUpActivity.java
+++ app/src/main/java/com/wess58/artissans/SignUpActivity.java
@@ -2,12 +2,28 @@ package com.wess58.artissans;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity {
+    @BindView(R.id.SinguptextView) TextView mSignUpTextView;
+    @BindView (R.id.FirstName) EditText mFirstName;
+    @BindView(R.id.LastName) EditText mLastName;
+    @BindView(R.id.UsernameSignUp) EditText mUsernameSignUp;
+    @BindView(R.id.emailtext) EditText mEmailText;
+    @BindView(R.id.Phone) EditText mPhone;
+    @BindView(R.id.signUp) Button mSignUpButton;
+    @BindView(R.id.emailConfirm) TextView mEmailConfirmText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
     }
 }
