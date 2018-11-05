f07f102e5ba58642da557da8eaae0a9806fde32d
Wess58 <wesleymbate@gmail.com>
2018-11-05 17:15:17 +0300

    implemented Splash screen

--- app/src/main/java/com/wess58/artissans/SplashActivity.java
+++ app/src/main/java/com/wess58/artissans/SplashActivity.java
@@ -1,13 +1,33 @@
 package com.wess58.artissans;
 
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SplashActivity extends AppCompatActivity {
-
-    @Override
+@BindView(R.id.splashText) TextView mSplashText;    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+
+        Typeface lobsterFonts = Typeface.createFromAsset(getAssets(), "fonts/Lobster_Two/LobsterTwo-Regular.ttf");
+        mSplashText.setTypeface(lobsterFonts);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
     }
 }
