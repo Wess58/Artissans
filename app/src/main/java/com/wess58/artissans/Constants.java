81d2d408bb9f0ca5e19c2f8d1a3a9ea9b5aa2fe2
Wess58 <wesleymbate@gmail.com>
2018-11-11 19:25:39 +0300

    created contstants class

---
+++ app/src/main/java/com/wess58/artissans/Constants.java
@@ -0,0 +1,8 @@
+package com.wess58.artissans;
+
+public class Constants {
+
+    public static final String ARTSY_API_KEY = BuildConfig.ARTSY_API_KEY;
+    public static final String ARTSY_BASE_URL = "https://api.artsy.net/api/artworks";
+    public static final String YELP_KEY_QUERY_PARAMETER = "key";
+}
