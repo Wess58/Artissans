01adebada8b0fc0417ffb35934409ea896982ffb
Wess58 <wesleymbate@gmail.com>
2018-11-21 19:55:05 +0300

    inflated logout item to overflow menu

--- app/src/main/java/com/wess58/artissans/ui/NewsActivity.java
+++ app/src/main/java/com/wess58/artissans/ui/NewsActivity.java
@@ -43,6 +43,26 @@ public class NewsActivity extends AppCompatActivity {
         moveTaskToBack(false);
     }
 
+    //Creating and Inflating an Overflow Menu START
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return  super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_logout){
+            logout();
+            return true;
+        }
+            return super.onOptionsItemSelected(item);
+    }
+
+    //END
+
 
 
 
