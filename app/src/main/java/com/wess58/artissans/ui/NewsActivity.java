18da72ccddb9a1b8fe7a3d0c7a98c1ac3133ca2d
Wess58 <wesleymbate@gmail.com>
2018-11-21 19:54:05 +0300

    removed backPress function

--- app/src/main/java/com/wess58/artissans/ui/NewsActivity.java
+++ app/src/main/java/com/wess58/artissans/ui/NewsActivity.java
@@ -1,11 +1,18 @@
 package com.wess58.artissans.ui;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.wess58.artissans.R;
 import com.wess58.artissans.adapter.NewsListAdapter;
 import com.wess58.artissans.models.News;
@@ -19,13 +26,24 @@ import butterknife.ButterKnife;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
+import android.view.Menu;
 
 public class NewsActivity extends AppCompatActivity {
     public static final String TAG = NewsActivity.class.getSimpleName();
     @BindView(R.id.recyclerNewsView) RecyclerView mRecyclerNewsView;
+    @BindView(R.id.newsTextView) TextView mNewsTextView;
     private NewsListAdapter mAdapter;
 
     public ArrayList<News> mNews = new ArrayList<>();
+
+
+    //Activity doesn't go to previous activity
+    @Override
+    public  void onBackPressed(){
+        moveTaskToBack(false);
+    }
+
+
 
 
     @Override
@@ -33,9 +51,14 @@ public class NewsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
         ButterKnife.bind(this);
+
+        //TOOLBAR INFLATE
+        Toolbar toolbar = findViewById(R.id.newstoolbar);
+        setSupportActionBar(toolbar);
         getNews();
 
-
+        Typeface lobsterFonts = Typeface.createFromAsset(getAssets(), "fonts/Lobster_Two/LobsterTwo-Regular.ttf");
+        mNewsTextView.setTypeface(lobsterFonts);
     }
 
     private void getNews() {
@@ -71,4 +94,7 @@ public class NewsActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
 }
