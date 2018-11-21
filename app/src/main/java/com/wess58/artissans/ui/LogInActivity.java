1e0db94fe468fb71d0b90be4c125466bcfd53222
Wess58 <wesleymbate@gmail.com>
2018-11-21 11:25:00 +0300

    Authenticated users

--- app/src/main/java/com/wess58/artissans/ui/LogInActivity.java
+++ app/src/main/java/com/wess58/artissans/ui/LogInActivity.java
@@ -2,6 +2,7 @@ package com.wess58.artissans.ui;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,20 +10,30 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.wess58.artissans.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class LogInActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @BindView(R.id.Username) EditText mUsername;
-    @BindView(R.id.editText2) EditText mPassword;
+    @BindView(R.id.Email) EditText mEmail;
+    @BindView(R.id.password1) EditText mPassword;
     @BindView(R.id.button) Button mLoginButton;
     @BindView(R.id.checkBox) CheckBox mStayLoggedin;
     @BindView(R.id.loginText) TextView mLoginTextView;
     @BindView(R.id.signUpLink) TextView mSignUpLink;
+
+    private FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,25 +41,59 @@ public class LogInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_log_in);
         ButterKnife.bind(this);
 
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+
         Typeface oldEnglishFonts = Typeface.createFromAsset(getAssets(), "fonts/Reem_Kufi/ReemKufi-Regular.ttf");
         mLoginTextView.setTypeface( oldEnglishFonts);
 
-        mLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LogInActivity.this, NewsActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        mSignUpLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
-                startActivity(intent);
-            }
-        });
+        mLoginButton.setOnClickListener(this);
+        mSignUpLink.setOnClickListener(this);
 
 
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mSignUpLink){
+            Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        if (v == mLoginButton){
+                SignInUser();
+        }
+
+    }
+
+    private void SignInUser() {
+
+        final String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(LogInActivity.this, NewsActivity.class);
+                            startActivity(intent);
+                            finish();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LogInActivity.this, "Invalid Credentials.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+
+    }
+
+
 }
