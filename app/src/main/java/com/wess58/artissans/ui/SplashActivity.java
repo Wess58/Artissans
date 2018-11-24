053a8f166ae98116a03c48ef2328797fe8d0f023
Wess58 <wesleymbate@gmail.com>
2018-11-24 23:05:31 +0300

    added transition animation uptodown - SplashActivity

--- app/src/main/java/com/wess58/artissans/ui/SplashActivity.java
+++ app/src/main/java/com/wess58/artissans/ui/SplashActivity.java
@@ -5,16 +5,27 @@ import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.wess58.artissans.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+Animation uptodown,downtoup;
+
 @BindView(R.id.splashText) TextView mSplashText;
 @BindView(R.id.footer) TextView mFooter;
+@BindView(R.id.uptodown) LinearLayout mUptodown;
+@BindView(R.id.downtoup) LinearLayout mDowntoup;
 
 
 @Override
@@ -22,6 +33,16 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         ButterKnife.bind(this);
+
+    uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
+    downtoup = AnimationUtils.loadAnimation(this, R.anim.downtoup);
+    mUptodown.setAnimation(uptodown);
+    mDowntoup.setAnimation(downtoup);
+
+    //<--- Rotate image START
+   /
+
+    //Rotate image END --->
 
         Typeface lobsterFonts = Typeface.createFromAsset(getAssets(), "fonts/Lobster_Two/LobsterTwo-Regular.ttf");
         mSplashText.setTypeface(lobsterFonts);
@@ -37,6 +58,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },3000);
+        },4000);
     }
 }
