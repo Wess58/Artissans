7bf51cd96b2e27cf18418c4292ac2fe188b75b56
Wess58 <wesleymbate@gmail.com>
2018-11-27 13:07:46 +0300

    fragment xmls for respective Fragments

--- app/src/main/java/com/wess58/artissans/Constants.java
+++ app/src/main/java/com/wess58/artissans/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final String NEWS_API_KEY = BuildConfig.NEWS_API_KEY;
     public static final String NEWS_BASE_URL = "https://api.harvardartmuseums.org/object?classification=(not assigned)|Paintings|Sculpture|Drawings&size=50&";
     public static final String NEWS_KEY_QUERY_PARAMETER = "key";
+    public static final String SOURCE_SAVED = "saved";
 }
