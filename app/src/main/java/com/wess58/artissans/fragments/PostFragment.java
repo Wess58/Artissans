35b86c7f35ed6cde0b83dc79e11548b48ad17a4b
Wess58 <wesleymbate@gmail.com>
2018-11-28 19:27:27 +0300

    created OnClickListeners to Access Camera Hardware - PostFragment.java

--- app/src/main/java/com/wess58/artissans/fragments/PostFragment.java
+++ app/src/main/java/com/wess58/artissans/fragments/PostFragment.java
@@ -2,28 +2,43 @@ package com.wess58.artissans.fragments;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.InputFilter;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.wess58.artissans.R;
+
+import butterknife.BindView;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PostFragment extends Fragment implements View.OnClickListener {
+public class PostFragment extends Fragment {
 
     private static final int REQUEST_IMAGE_CAPTURE = 111;
+    private static final int REQUEST_SELECT_IMAGE = 1;
+    private ImageButton mImageButton;
+    @BindView(R.id.postButton) Button mPostButton;
+    @BindView(R.id.LocationEditText) EditText mLocationEditText;
+    @BindView(R.id.captionEditText) EditText mCaptionEditText;
+    @BindView(R.id.PriceEditText) EditText mPriceEditText;
+    @BindView(R.id.galleryicon) ImageView mGalleryIcon;
+    @BindView(R.id.cameraicon) ImageView mCameraIcon;
+    public Uri uri;
 
-        public PostFragment() {
+    public PostFragment() {
         // Required empty public constructor
     }
 
@@ -31,69 +46,73 @@ public class PostFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false);
+        View view = inflater.inflate(R.layout.fragment_post, container, false);
+        mCaptionEditText = view.findViewById(R.id.captionEditText);
+        mLocationEditText = view.findViewById(R.id.LocationEditText);
+        mPriceEditText = view.findViewById(R.id.PriceEditText);
+        mImageButton = view.findViewById(R.id.imageButton);
+        mGalleryIcon = view.findViewById(R.id.galleryicon);
+        mCameraIcon = view.findViewById(R.id.cameraicon);
 
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
-
-
-
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
-            inflater.inflate(R.menu.post_menu, menu);
-            return;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_post:
+        //<--- CLICKLISTENERS FOR IMAGE PICKING
+        mGalleryIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImageFromGallery();
+            }
+        });
+        mCameraIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 onLaunchCamera();
-            default:
-                break;
-        }
-        return false;
+            }
+        });
+
+        //CLICKLISTENERS FOR IMAGE PICKING END --->
+
+
+
+
+        mCaptionEditText.setFilters(new InputFilter[]{ new InputFilter.LengthFilter(100) });
+        mLocationEditText.setFilters(new InputFilter[]{ new InputFilter.LengthFilter(20) });
+        mPriceEditText.setFilters(new InputFilter[]{ new InputFilter.LengthFilter(10) });
+
+
+
+
+        return view;
+
     }
 
-    public void onLaunchCamera() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
-
-    @Override
-    public void onClick(View v) {
-
-    }
-
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK) {
-//            Bundle extras = data.getExtras();
-//            Bitmap imageBitmap = (Bitmap) extras.get("data");
-//            mImageLabel.setImageBitmap(imageBitmap);
-//            encodeBitmapAndSaveToFirebase(imageBitmap);
+    //selectImageFromGallery Intent to choose image straight from gallery
+//    private void selectImageFromGallery () {
+//        Intent selectFromGallery = new Intent(Intent.ACTION_GET_CONTENT);
+//        selectFromGallery.setType("image/*");
+//        startActivityForResult(selectFromGallery, REQUEST_SELECT_IMAGE);
+//    }
+//
+//    // Launch Camera to Take pics
+//    public void onLaunchCamera() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//
 //        }
 //    }
+//
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == REQUEST_SELECT_IMAGE && resultCode == RESULT_OK && data != null) {
+//            uri = data.getData();
+//            mImageButton.setImageURI(uri);
+//        }
 
-//    public void encodeBitmapAndSaveToFirebase(Bitmap bitmap) {
-//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-//        String imageEncoded = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
-//        DatabaseReference ref = FirebaseDatabase.getInstance()
-//                .getReference(Constants.FIREBASE_CHILD_RESTAURANTS)
-//                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-//                .child(mNews.getPushId())
-//                .child("imageUrl");
-//        ref.setValue(imageEncoded);
-//    }
+
+
+
+    }
 }
